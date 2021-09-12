2c9086f30552deda8567c210fd660949ee65c2f7
Sagar Kumar <mtech.sagarkumar@gmail.com>
2021-09-12 18:10:31 +0530

    styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 
 import React from 'react';
 import Typography from '@material-ui/core/Typography'
-// import {Box,Container }from '@material-ui/core/Box'
+import {Box,Container }from '@material-ui/core/Box'
+import {makeStyles} from '@material-ui/core/styles'
 
  
 
@@ -10,6 +11,7 @@ export default function App() {
   return (
     <div>
       <Typography variant="h3" color="secondary"> Hello World! </Typography>
+      <Typography variant="subtitle1" color="secondary"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, et facilis rem quidem nisi beatae delectus est dolore corrupti ab voluptatibus cupiditate consequuntur earum odit quisquam perferendis sint modi maxime id atque pariatur quo. </Typography>
       
     </div>
   )
