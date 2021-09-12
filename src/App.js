1457ab822c32c73219377123a4c70b130e8880af
Sagar Kumar <mtech.sagarkumar@gmail.com>
2021-09-12 21:06:43 +0530

    matrial ui page concept

--- src/App.js
+++ src/App.js
@@ -1,53 +1,27 @@
 import React from "react";
-import Typography from "@material-ui/core/Typography";
-import { Box, Container } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
-import { mergeClasses } from "@material-ui/styles";
+import { Typography, Container, Button, Paper, Box } from "@material-ui/core";
 
-const useStyles = makeStyles((theme)=>({
-  container:{
-    width:"100vw",
-    height:"100vh",
-    display:"flex",
-    alignItems:"flex-start"
-    
-  },
-  mybox: {
-    width: 300,
-    backgroundColor: theme.palette.primary.light,
-    color: "white",
-    paddingTop: 50,
-    borderRadius:4,
-    boxShadow:"0px 10px 10px rgba(0,0,0,0.1)"
-  },
-}));
-
-export default function App() {
-  const classes = useStyles();
-
+function App() {
   return (
-    <Box className={classes.container}>
-      <Box className={classes.mybox}>
-        <Typography variant="h4">Heading 1</Typography>
-        <Typography variant="subtitle1">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae,
-          corrupti.
-        </Typography>
-      </Box>
-      <Box className={classes.mybox}>
-        <Typography variant="h4">Heading 2</Typography>
-        <Typography variant="subtitle1">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae,
-          corrupti.
-        </Typography>
-      </Box>
-      <Box className={classes.mybox}>
-        <Typography variant="h4">Heading 3</Typography>
-        <Typography variant="subtitle1">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae,
-          corrupti.
-        </Typography>
-      </Box>
-    </Box>
+    <Container maxWidth="sm">
+      <Paper square elevation={4} varinat='outlined'>
+        <Box p={2}>
+          <Typography variant="h3"> Read Docs</Typography>
+          <Typography variant="subtitle1">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos dolorum
+            eligendi natus veritatis! Inventore perferendis quaerat ratione
+            perspiciatis laudantium recusandae veritatis quod? Molestias iure
+            nam exercitationem delectus, quasi perferendis sed. Nam suscipit
+            blanditiis vel?
+          </Typography>
+          <Button variant="contained" color="primary">
+        Learn More
+      </Button>
+        </Box>
+      </Paper>
+     
+    </Container>
   );
 }
+
+export default App;
