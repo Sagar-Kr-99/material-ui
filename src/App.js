363802c9b7d66be1015bcd801159e99f9caa1a62
Sagar Kumar <mtech.sagarkumar@gmail.com>
2021-09-12 18:21:29 +0530

    Box

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+import { Box, Container } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { mergeClasses } from "@material-ui/styles";
 
-import React from 'react';
-import Typography from '@material-ui/core/Typography'
-import {Box,Container }from '@material-ui/core/Box'
-import {makeStyles} from '@material-ui/core/styles'
-
- 
+const useStyles = makeStyles({
+  mybox: {
+    width: 300,
+    backgroundColor: "white",
+    color:"red",
+    paddingTop:50
+  },
+});
 
 export default function App() {
- 
+  const classes = useStyles();
+
   return (
-    <div>
-      <Typography variant="h3" color="secondary"> Hello World! </Typography>
-      <Typography variant="subtitle1" color="secondary"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, et facilis rem quidem nisi beatae delectus est dolore corrupti ab voluptatibus cupiditate consequuntur earum odit quisquam perferendis sint modi maxime id atque pariatur quo. </Typography>
+    
+    <Box>
+    <Box className={classes.mybox}>
       
-    </div>
-  )
+        <Typography variant="h3" color="secondary">
+          Hello World!
+        </Typography>
+        <Typography variant="subtitle1" color="secondary">
+          {" "}
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, et
+          facilis rem quidem nisi beatae delectus est dolore corrupti ab
+          voluptatibus cupiditate consequuntur earum odit quisquam perferendis
+          sint modi maxime id atque pariatur quo.{" "}
+        </Typography>
+     
+    </Box>
+    </Box>
+  );
 }
-
-
-
-
